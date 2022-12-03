ff55bd358cd587e565983f910b958081a6bf47e8
atakieddin <ameertakieddin@Ameers-MBP.home>
2022-12-03 12:46:16 -0500

    Add more debug logs

--- telegram_bot/telegram_bot/features/add_to_playlist/handlers.py
+++ telegram_bot/telegram_bot/features/add_to_playlist/handlers.py
@@ -1,8 +1,13 @@
 """Add music to playlist"""
+import logging
 from .youtube_music import utils as yt_utils
 from .spotify_playlist import utils as spot_utils
 
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.DEBUG
+)
 
+logger = logging.getLogger(__name__)
 
 def add_song(update, _):
     """Add song to playlist after determining type"""
@@ -14,15 +19,19 @@ def add_song(update, _):
         yt_details = yt_utils.get_details(url=url)
         artist = yt_details['author']
         title = yt_details['title']
+        
+        logging.debug("This is the artist and the title: %s, %s", artist, title)
 
         spotify_url = spot_utils.get_song(artist=artist, title=title)
         msg = "This looks like a youtube URL, adding the closest song I could find: " + spotify_url
 
+        logging.debug("This is the spotify_url %s", spotify_url)
 
     else:
         spotify_url = url
         msg = "Added song to playlist!"
 
+    logging.debug("We are about to add it")
     spot_utils.add_song(song=spotify_url)
 
     update.message.reply_text(msg)
